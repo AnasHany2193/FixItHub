0737bb55782dce8ccf6385fdd4401c53ac59a29e
Anas Hany <anashany2193@gmail.com>
2025-02-01 04:36:35 +0200

    Setup Use Context for dark-light theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
 import { Routes, Route } from "react-router";
+import { useApp } from "./contexts/AppContext";
 
 function App() {
+  const { darkMode, changeMode } = useApp();
+
   return (
-    <main className="flex min-h-screen">
+    <main className={`${darkMode ? "dark" : ""} flex min-h-screen`}>
       <Routes>
-        <Route index path="/" element={<div>Home Page</div>} />
+        <Route
+          index
+          path="/"
+          element={
+            <div className="dark:bg-slate-800 dark:text-gray-400">
+              Home Page
+              <button
+                onClick={changeMode}
+                className="fixed p-2 text-xl bg-green-100 rounded-full bottom-4 left-4 dark:bg-gray-800 cursor-pointer hover:drop-shadow-lg"
+              >
+                {darkMode ? "🌞" : "🌙"}
+              </button>
+            </div>
+          }
+        />
         <Route index path="/about" element={<div>About Page</div>} />
       </Routes>
     </main>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 
+import { AppProvider } from "./contexts/AppContext.jsx";
 import QueryProvider from "./providers/QueryProvider.jsx";
 
 import "./index.css";
@@ -9,7 +10,9 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <QueryProvider>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </QueryProvider>
   </BrowserRouter>
 );
